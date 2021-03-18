bd73a9a8e4d73b3a8425d5727246b08fe6ec1180
Rjeey <licanomega@gmail.com>
2021-03-18 05:26:07 +0300

    change mvc config

--- src/main/java/com/artem/springwebsocket_lightroom/config/MyWebMvcConfig.java
+++ src/main/java/com/artem/springwebsocket_lightroom/config/MyWebMvcConfig.java
@@ -3,15 +3,15 @@ package com.artem.springwebsocket_lightroom.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MyWebMvcConfig extends WebMvcConfigurerAdapter {
+public class MyWebMvcConfig implements WebMvcConfigurer {
 
     @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName(
-                        "forward:/rooms.html");
-            }
-
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(
+                "forward:/rooms.html");
+    }
 }
